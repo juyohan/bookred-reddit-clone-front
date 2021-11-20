a100c313bb4ec577ad609cf6bf3b7cbcefa7e599
juyohan <>
2021-11-20 17:45:15 +0900

    더보기 버튼 구현

--- src/Component/Feed/FeedCard/CardFeed/CardFeed.jsx
+++ src/Component/Feed/FeedCard/CardFeed/CardFeed.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from "prop-types";
 // css
 import {
     CardContent,
     CardFeedDesc,
     CardFeedTitle,
-    CardFeedWrapper,
+    CardFeedWrapper, UnfoldBtn,
 } from "./CardFeed.styles";
 // Component
 import CardImage from "./CardImage";
 
 const CardFeed = ({title, feedContent, fileNames}) => {
-    // console.log(fileNames);
+    const [open, setOpen] = useState(false);
+
+    const openContent = (e) => {
+        e.preventDefault();
+        setOpen(true);
+    }
+
     return (
         <CardFeedWrapper>
             <CardFeedTitle>
@@ -19,15 +25,18 @@ const CardFeed = ({title, feedContent, fileNames}) => {
                     {title}
                 </h3>
             </CardFeedTitle>
-            <CardFeedDesc style={{
-                maxHeight : '120px'
-            }}
-            >
-                 <CardContent dangerouslySetInnerHTML={{
-                     __html : feedContent
-                 }}>
-                 </CardContent>
-                dlkjsdf
+            <CardFeedDesc>
+                <CardContent dangerouslySetInnerHTML={{
+                    __html: feedContent
+                }}
+                             open={open}
+                />
+                <UnfoldBtn type={"button"}
+                           onClick={openContent}
+                           open={open}
+                >
+                    ... 더보기
+                </UnfoldBtn>
             </CardFeedDesc>
             {
                 fileNames.length > 0 ?
@@ -39,9 +48,9 @@ const CardFeed = ({title, feedContent, fileNames}) => {
 }
 
 CardFeed.propTypes = {
-    title : PropTypes.string,
-    content : PropTypes.string,
-    fileNames : PropTypes.object
+    title: PropTypes.string,
+    content: PropTypes.string,
+    fileNames: PropTypes.object
 }
 
 export default CardFeed;
